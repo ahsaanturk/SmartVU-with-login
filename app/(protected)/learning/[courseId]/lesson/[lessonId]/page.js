932b429a9ba4fe5103079e1c0e58ac8ce2e839fa
Ahsaan <ahsaanturk@gmail.com>
2026-01-03 10:27:56 +0500

    feat: Mobile responsive dashboard, admin layout fixes, and Youtube autoplay

--- app/(protected)/learning/[courseId]/lesson/[lessonId]/page.js
+++ app/(protected)/learning/[courseId]/lesson/[lessonId]/page.js
@@ -189,7 +189,7 @@ export default function LessonPage({ params }) {
                                 {videoId && (
                                     <iframe
                                         style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                                        src={`https://www.youtube.com/embed/${videoId}`}
+                                        src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
                                         title="YouTube video player"
                                         frameBorder="0"
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
